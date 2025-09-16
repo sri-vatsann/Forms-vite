b3a56bb594c7ccb05a1b9ec73c87e3d3140a721c
ssri-vatsann <srivatsan2910@gmail.com>
2025-09-16 16:01:33 +0530

    d

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 const Form = ( {handleFormDataChange, handleSubmit, formData, editIndex} ) => {
+
   //console.log("props",props);
   return (
+
     <div className='p-6 m-auto max-w-2xl'>
     <h1 className='text-2xl font-semibold mb-4'>Forms and Table Example </h1>
+
     <form className='border rounded-lg p-4' onSubmit={handleSubmit}>
+      
         <input className="w-full p-2 border rounded mb-2" type="text" placeholder='Name'
          onChange={(e)=> {
           const {value} = e.target;
